6490a278e9ae6d786222781a0755b3eb911c1b5c
conner martin <connersmartin@gmail.com>
2020-04-11 23:02:37 -0700

    Added basic auth functionality

--- SpotShare/wwwroot/js/site.js
+++ SpotShare/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$("#authBtn").click(function () {
+    location.href = 'Share/Auth';
+});
+if (document.cookie.indexOf('spotauthtoke') !== -1) {
+    $("#initLink").removeAttr("hidden");
+}
